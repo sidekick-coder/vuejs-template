6d92ae0d8ad14bc1586122d50bad578b8c3ed596
Henrique <henryqueoli@hotmail.com>
2023-05-12 12:52:02 -0300

    feat: initial setup

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Logo from './assets/logo.svg?raw'
+</script>
+<template>
+  <div class="h-screen w-screen flex items-center justify-center bg-gray-900 text-white" >
+    <div class="text-center">
+      <div v-html="Logo" class="w-32 h-32 mx-auto" />
+
+      <div class="text-xl">
+        Sidekick-coder template
+      </div>
+
+    </div>
+  </div>
+</template>
